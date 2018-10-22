eabd69e5a12b06f59c334de2e839182f3920de3f
Marek Jastrzebski <marek.jastrzebski@outlook.com>
2018-10-22 10:08:34 +0200

    bootstrap sb admin added

--- src/main/java/pl/coderslab/sportsbet2/AppConfig.java
+++ src/main/java/pl/coderslab/sportsbet2/AppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import pl.coderslab.sportsbet2.converter.DateConverter;
 
@@ -16,25 +17,24 @@ public class AppConfig extends WebMvcConfigurerAdapter  {
         return new DateConverter();
     }
 
-
-
     @Override
     public void addFormatters (FormatterRegistry registry) {
         registry.addConverter(getDateConverter());
     }
 
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler(
-//                "/webjars/**",
-//                "/img/**",
-//                "/css/**",
-//                "/js/**", "/vendor/**")
-//                .addResourceLocations(
-//                        "classpath:/static/img/",
-//                        "classpath:/static/css/",
-//                        "classpath:/static/js/",
-//                        "classpath:/static/vendor/**");
-//    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/webjars/**",
+                "/img/**",
+                "/css/**",
+                "/js/**",
+                "/vendor/**")
+                .addResourceLocations(
+                        "classpath:/static/img/",
+                        "classpath:/static/css/",
+                        "classpath:/static/js/",
+                        "classpath:/static/vendor/");
+    }
 
 }
