09c7cc191a5ef510911d929a228bb2fde7c78474
marek <marek.jastrzebski@outlook.com>
2019-12-04 15:37:39 +0100

    Password encryption added to setter

--- src/main/java/pl/bets365mj/user/User.java
+++ src/main/java/pl/bets365mj/user/User.java
@@ -3,6 +3,7 @@ package pl.bets365mj.user;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import pl.bets365mj.coupon.Coupon;
 import pl.bets365mj.fixtureMisc.Country;
 import pl.bets365mj.role.Role;
@@ -26,51 +27,35 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-
     @Column(nullable = false, unique = true)
     @UsernameUnique
     private String username;
-
     @NotNull
     private String password;
-
-    @NotNull
     private String firstName;
-
-    @NotNull
     private String lastName;
-
-    @NotNull
     private boolean adult;
-
-    @Email
-    @Column(nullable = false, unique = true)
+    @Email @Column(unique = true)
     private String mail;
-
     @OneToOne(cascade = CascadeType.ALL)
     private Wallet wallet;
-
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Coupon> coupons = new ArrayList<>();
-
     private boolean enabled = false;
-
     @ManyToMany(fetch = FetchType.EAGER)
     private Set<Role> roles;
-
     private String street;
-
     private String city;
-
     private String zip;
-
     @OneToOne
     private Country country;
-
     private boolean dataProcessingAcknowledgement = false;
-
     private boolean active = true;
 
     public User(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+    }
+
+    public void setPassword(String password) {
+        this.password = BCrypt.hashpw(password, BCrypt.gensalt());
     }
 }
