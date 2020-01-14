cfec807a704062c43f1e870791b81ae59d4fbede
Marek Jastrzębski <marek.jastrzebski@outlook.com>
2020-01-14 21:38:48 +0100

    h2 database added as a source of test data

--- src/test/java/pl/bets365mj/SportsBet2ApplicationTests.java
+++ src/test/java/pl/bets365mj/SportsBet2ApplicationTests.java
@@ -3,10 +3,13 @@ package pl.bets365mj;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration (classes = SportsBet2Application.class)
 public class SportsBet2ApplicationTests {
 
     @Test
--- src/test/java/pl/bets365mj/fixture/FixtureRepositoryTest.java
+++ src/test/java/pl/bets365mj/fixture/FixtureRepositoryTest.java
@@ -2,14 +2,30 @@ package pl.bets365mj.fixture;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@TestPropertySource (locations = "/application-test.properties")
 public class FixtureRepositoryTest {
+
+    @Autowired
+    FixtureRepository fixtureRepository;
+
+    @Value("${foo}")
+    String foo;
+
+    @Test
+    public void fooShoulBeEqualBar(){
+        assertEquals(foo, "bar");
+        System.out.println(foo);
+    }
 
     @Test
     public void findTop5ByHomeTeam() {
